be29b5d7f4b837e66310dfe9a09f9bc79ddf25c2
Jie-Li-uob <78802105+Jie-Li-uob@users.noreply.github.com>
2021-05-04 20:46:18 +0800

    Delete taste.sql

--- database/taste.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `taste` (`Taste`, `Tid`) VALUES ('Sweet', '1');
-INSERT INTO `taste` (`Taste`, `Tid`) VALUES ('Salty', '2');
-INSERT INTO `taste` (`Taste`, `Tid`) VALUES ('Tender', '3');
-INSERT INTO `taste` (`Taste`, `Tid`) VALUES ('Spicy', '4');
-INSERT INTO `taste` (`Taste`, `Tid`) VALUES ('Sour', '5');
-INSERT INTO `taste` (`Taste`, `Tid`) VALUES ('Crispy', '6');
-INSERT INTO `taste` (`Taste`, `Tid`) VALUES ('Light', '7');
